f7acbbeddecc3454f916fa0fd5296f62e3a0b9d0
dieverson-melo <164172530+dieverson-melo@users.noreply.github.com>
2025-03-13 11:19:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+        const aumentaFonteBotao = document.getElementById('aumentar - fonte');
+
+        let tamanhoAtualfonte = 1;
+
+        aumentaFonteBotao.addEventListener('click', function(){
+            tamanhoAtualfonte +=;
+
+    })
+    
+})
