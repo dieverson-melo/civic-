706975c45d2477dd50746f325bb4e88e07d36a43
dieverson-melo <164172530+dieverson-melo@users.noreply.github.com>
2025-03-27 16:40:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-        const aumentaFonteBotao = document.getElementById('aumentar - fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-        let tamanhoAtualfonte = 1;
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
-        aumentaFonteBotao.addEventListener('click', function(){
-            tamanhoAtualfonte +=;
+    });
 
-    })
-    
-})
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
